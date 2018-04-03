e2a6a38fe3d39566edec04d6429823710b498770
Matheus Heiden <matheusheiden@outlook.com>
2018-04-03 19:23:18 -0300

    Adicionado especificação do gals e código gerado

---
+++ src/main/java/gals/Constants.java
@@ -0,0 +1,52 @@
+package gals;
+
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_IDENTIFICADOR = 2;
+    int t_CONSTANTE_INTEIRA = 3;
+    int t_CONSTANTE_REAL = 4;
+    int t_CONSTANTE_CARACTER = 5;
+    int t_BOOL = 6;
+    int t_CONSTS = 7;
+    int t_DEF = 8;
+    int t_END = 9;
+    int t_EXECUTE = 10;
+    int t_FALSE = 11;
+    int t_FLOAT = 12;
+    int t_GET = 13;
+    int t_IFFALSE = 14;
+    int t_IFTRUE = 15;
+    int t_INPUT = 16;
+    int t_INT = 17;
+    int t_PRINT = 18;
+    int t_PRINTLN = 19;
+    int t_SET = 20;
+    int t_STR = 21;
+    int t_TRUE = 22;
+    int t_TYPES = 23;
+    int t_VAR = 24;
+    int t_WHILEFALSE = 25;
+    int t_WHILETRUE = 26;
+    int t_TOKEN_27 = 27; //"+"
+    int t_TOKEN_28 = 28; //"-"
+    int t_TOKEN_29 = 29; //"*"
+    int t_TOKEN_30 = 30; //"/"
+    int t_TOKEN_31 = 31; //":"
+    int t_TOKEN_32 = 32; //"("
+    int t_TOKEN_33 = 33; //")"
+    int t_TOKEN_34 = 34; //"="
+    int t_TOKEN_35 = 35; //":="
+    int t_TOKEN_36 = 36; //","
+    int t_TOKEN_37 = 37; //"&&"
+    int t_TOKEN_38 = 38; //"||"
+    int t_TOKEN_39 = 39; //"!"
+    int t_TOKEN_40 = 40; //"!="
+    int t_TOKEN_41 = 41; //"<"
+    int t_TOKEN_42 = 42; //"<="
+    int t_TOKEN_43 = 43; //">"
+    int t_TOKEN_44 = 44; //">="
+
+}
